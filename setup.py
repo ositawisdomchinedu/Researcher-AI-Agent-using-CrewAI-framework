81e0024917878ef9e4e197d850404a3a600bd815
Osita Wisdom Chinedu <chinwizzy@gmail.com>
2025-06-25 18:39:41 +0100

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Reseacher AI Agent',
+    version= '0.0.0',
+    author= 'Osita Wisdom Chinedu',
+    author_email= 'chinwizzy@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
