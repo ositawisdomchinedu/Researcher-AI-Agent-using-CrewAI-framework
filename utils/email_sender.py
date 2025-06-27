d5af736b221a1affd2c4b4a42d91bf131e313e86
Osita Wisdom <chinwizzy@gmail.com>
2025-06-27 16:33:51 +0100

    Update email_sender.py

--- utils/email_sender.py
+++ utils/email_sender.py
@@ -4,7 +4,7 @@ from config.settings import SENDER_EMAIL, SENDER_PASSWORD
 
 def send_email_with_attachment(receiver_email, subject, body, attachment_path):
     try:
-        yag = yagmail.SMTP(user=SENDER_EMAIL, password=SENDER_PASSWORD, oauth2_file=False)
+        yag = yagmail.SMTP(user=SENDER_EMAIL, password=SENDER_PASSWORD)
         yag.send(to=receiver_email, subject=subject, contents=body, attachments=attachment_path)
         return "SUCCESS"
 
